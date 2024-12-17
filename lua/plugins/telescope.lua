bfabd6ed83e8cd76dbabecba0fdaa478ab771c7e
chengxiaoran <15632691603@163.com>
2024-12-17 13:35:34 +0800

    feat(custom): 修改telescope配置，处理lazyVim升级后的问题

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,25 @@
-local telescopeConfig = require("telescope.config")
-
--- 克隆默认的望远镜配置
-local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
-
--- 搜索隐藏/点文件
-table.insert(vimgrep_arguments, "--hidden")
-
 return {
-  {
-    "telescope.nvim",
-    opts = {
+  "nvim-telescope/telescope.nvim",
+  config = function()
+    -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
+    local telescopeConfig = require("telescope.config")
+
+    local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+    table.insert(vimgrep_arguments, "--hidden")
+
+    require("telescope").setup({
       defaults = {
+        -- 允许 live_grep 在.文件搜索文本
         vimgrep_arguments = vimgrep_arguments,
       },
-    },
-  },
+      pickers = {
+        find_files = {
+          -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+          -- 允许搜索.文件
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+      },
+    })
+  end,
 }
--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,26 +1,20 @@
 return {
-  -- {
-  --   "gbprod/yanky.nvim",
-  --   event = "VeryLazy",
-  --   opts = {
-  --     system_clipboard = {
-  --       sync_with_ring = true,
-  --     },
-  --   },
-  -- },
-
   -- eslint
   { import = "lazyvim.plugins.extras.linting.eslint" },
   -- prettier
   { import = "lazyvim.plugins.extras.formatting.prettier" },
   -- 开启vscode支持
   { import = "lazyvim.plugins.extras.vscode" },
+  -- telescope
+  { import = "lazyvim.plugins.extras.editor.telescope" },
   -- yanky
   { import = "lazyvim.plugins.extras.coding.yanky" },
   -- codeium
   { import = "lazyvim.plugins.extras.ai.codeium" },
   -- diff
   { import = "lazyvim.plugins.extras.editor.mini-diff" },
+  -- markdown
+  { import = "lazyvim.plugins.extras.lang.markdown" },
   {
     -- TODO 高亮注释
     "folke/todo-comments.nvim",
@@ -32,22 +26,6 @@ return {
       },
       search = {
         pattern = [[\b(KEYWORDS)\b]], -- 匹配，而不带额外的冒号。您可能会得到误报
-      },
-    },
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        -- telescope忽略这些文件夹下的文件
-        file_ignore_patterns = {
-          "node_modules",
-          "%.git/",
-          "%.DS_Store",
-          "%.hbuilderx",
-          "%.history",
-          "dist",
-        },
       },
     },
   },
