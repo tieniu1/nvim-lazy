d2b100a14f34ba61516ba5da45ed569f2c885cfc
chengxiaoran <root@LAPTOP-1Q1JADA0>
2024-12-18 22:59:13 +0800

    fix(关闭markdown文件的拼写检查，解决中文和部分英文波浪线问题): 关闭markdown文件的拼写检查，解决中文和部分英文波浪线问题

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+-- 自动命令在 VeryLazy 事件上自动加载
+-- 默认的自动命令始终设置：https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- 在此添加任何额外的自动命令
+
+-- markdown 文件关闭拼写检查
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
