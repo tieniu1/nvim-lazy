68ab833d426658bbf9535189f356ca943d826da2
chengxiaoran <15632691603@163.com>
2024-12-20 19:22:11 +0800

    fix(custom): 增加键盘映射

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,5 +12,13 @@ local map = LazyVim.safe_keymap_set
 -- 浮动窗口显示lsp报错
 map("n", "gl", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
 
--- 重新配置y，将复制内容写入系统剪切板
-map({ "n", "v" }, "y", '"+y')
+-- -- 重新配置y，将复制内容写入系统剪切板
+-- map({ "n", "v" }, "y", '"+y')
+
+-- 取消 alt+k和alt+j 移动代码
+vim.keymap.del("n", "<M-k>")
+vim.keymap.del("i", "<M-k>")
+vim.keymap.del("v", "<M-k>")
+vim.keymap.del("n", "<M-j>")
+vim.keymap.del("i", "<M-j>")
+vim.keymap.del("v", "<M-j>")
